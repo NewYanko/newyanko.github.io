3376864ea424ced503cc3e818c89a44e40f32c04
Yanko <yankothemain@gmail.com>
2024-10-03 16:02:00 +0100

    Add mouse click sound

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,28 @@
 import Paging from "./UI/Paging.js";
+import Utils from "./UI/Utils.js";
 
 $(() => {
   setTimeout(() => {
     HandleHash();
   }, 250);
   window.addEventListener("hashchange", HandleHash);
+
+  const MinClick = 100;
+  let LastMouseDown = 0;
+
+  $("a").on("mousedown", () => {
+    $("#click-in")[0].currentTime = 0;
+    $("#click-in")[0].play();
+    LastMouseDown = Date.now();
+  });
+
+  $("a").on("mouseup", async () => {
+    if (LastMouseDown + MinClick > Date.now())
+      await Utils.Delay(LastMouseDown + MinClick - Date.now());
+
+    $("#click-out")[0].currentTime = 0;
+    $("#click-out")[0].play();
+  });
 });
 
 function HandleHash() {
